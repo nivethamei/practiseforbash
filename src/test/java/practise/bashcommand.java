968b3fae757dbf473a12375ca4c90fd39962ddbb
nivethamei <nivetha.meiyazhagan@gmail.com>
2024-08-18 12:54:03 +0530

    secondcommit

--- src/test/java/practise/bashcommand.java
+++ src/test/java/practise/bashcommand.java
@@ -2,7 +2,7 @@ package practise;
 
 public class bashcommand {
 	public static void main(String[] args) {
-		System.out.println("practise done");
+		System.out.println("practise was done");
 	}
 
 }
